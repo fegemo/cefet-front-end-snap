3b8c327b72b8d5b9f559d420ea6649adc0c60ecb
Flavio Coutinho <fegemo@gmail.com>
2021-12-17 07:24:53 -0300

    Exercise 1: showing balloon

--- script/balaozinho-jquery.js
+++ script/balaozinho-jquery.js
@@ -1,2 +1,12 @@
 // Sugestão: mantenha este arquivo e o balaozinho-vanilla.js abertos
 //           lado a lado e vá "traduzindo" o código para usar jQuery
+let $marcacoes = $('.marcacao');
+let $balaozinho = $('#balaozinho');
+
+$marcacoes.on('mouseover', e => {
+  $balaozinho.html('qualquer coisa');
+});
+
+$marcacoes.on('mouseout', e => {
+  $balaozinho.html('');
+});
